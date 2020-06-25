9545c13ea604ccaa7377ee38556421e26c2a177b
Ali Syahidin <ali.syahidin@elevenia.co.id>
2020-06-25 17:04:45 +0700

    update on requirements

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom'
 import { useRouter } from 'next/router'
 import { gsap } from 'gsap'
 import clsx from 'clsx'
+import { LogoSVG } from 'components/Logo'
 
 const menu = [
   {
@@ -51,13 +52,13 @@ const MenuList = ({ show, setShow }) => {
     <div ref={menuElement} style={{ left: '100%', zIndex: 5 }} className="fixed top-0 h-screen w-screen flex bg-white py-24">
       <div className="hidden md:flex h-100 flex-1 items-center justify-center border-gray-400 border-r-2">
         <div className="px-10 flex flex-col items-start">
-          <div className="flex flex-col mb-12">
-            <img className="mb-8" src="/logo.svg" alt="Hakuhodo" />
+          <div className="flex flex-col items-center mb-12">
+            <LogoSVG className="mb-4" width={194} />
             <img src="/Hakuhodo.svg" alt="Hakuhodo" />
           </div>
           <p className="text-default uppercase menu-title" style={{ lineHeight: '28px' }}>
             <b style={{ color: '#CD697B' }}>H:THREE</b> Is a multinational <br />
-            communication firm who belives in equal <br />
+            communication firm who believes in equal <br />
             partnership, Supported with strong local talents.
             </p>
         </div>
--- components/Video.js
+++ components/Video.js
@@ -1,9 +1,42 @@
+import { useState, useRef, useEffect } from 'react'
 import ReactPlayer from 'react-player'
+import clsx from 'clsx'
 
 const Video = ({ src }) => {
+  const player = useRef(null)
+  const [played, setPlayed] = useState(false)
+  const [preview, setPreview] = useState(false)
+
+  const mouseLeave = () => {
+    if (!played) {
+      setPreview(false)
+      player.current.seekTo(0, 'seconds')
+    }
+  }
+
+  const playVideo = () => {
+    setPlayed(true)
+    player.current.seekTo(0, 'seconds')
+  }
+
+  useEffect(() => {
+    if (!played) {
+      console.log(player.current.getCurrentTime())
+    }
+  }, [])
+
   return (
-    <div style={{ position: 'relative' }}>
-      <ReactPlayer width="100%" height="100%" url={src} />
+    <div className={clsx([{ 'cursor-pointer': !played }])} onClick={playVideo} onMouseLeave={mouseLeave} onMouseEnter={() => !played && setPreview(true)} style={{ position: 'relative' }}>
+      <ReactPlayer
+        ref={player}
+        playing={played || preview}
+        width="100%"
+        height="100%"
+        url={src}
+        controls={played}
+        volume={1}
+        muted={!played}
+      />
     </div>
   )
 }
